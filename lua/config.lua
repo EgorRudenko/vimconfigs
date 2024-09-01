b09080dc42730f8ea97bbc7cf928ba60e80d54c3
Yehor <rudenko.egorka@gmail.com>
2024-09-01 21:40:42 +0200

    Toggleterm expansion

--- lua/config.lua
+++ lua/config.lua
@@ -35,15 +35,65 @@ require('lazy').setup({
   {'akinsho/toggleterm.nvim', version = "*", config = true},
 })
 
+
+
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<a-g>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+
+
+local normalTerminal = Terminal:new({
+  direction = "horizontal",
+})
+
+
+function _normterm_toggle()
+  normalTerminal:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<a-t>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+
+
+
+vim.cmd("autocmd BufEnter * if &buftype ==# 'terminal' | startinsert! | endif")
+
+
+
+
+
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
---  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
---  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
---  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
---  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>q', [[<C-\><C-n>:q<CR>]], opts)
 end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+
+
 
 vim.cmd('set rtp+=/usr/bin/fzf')
 vim.cmd('let g:NERDTreeWinPos = "right"')
